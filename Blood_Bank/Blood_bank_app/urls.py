e9a093de8923f1856b43f5faf2aabc26dacfe2ee
Jelan <jelanmathewjames1234@gmail.com>
2021-10-06 22:25:00 +0530

    ajax applied to all pages set as responsive

--- Blood_Bank/Blood_bank_app/urls.py
+++ Blood_Bank/Blood_bank_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
      path('',views.home,name="home"),
      path('login',views.log_in,name="login"),
-     path('signin',views.sign_in,name="signin"),
+     path('signup',views.sign_up,name="signin"),
      path('display',views.display,name="display"),
      path('add-donor',views.adddonor,name="add-donor"),
      path('logout',views.logout,name="logout")
--- Blood_Bank/Blood_bank_app/views.py
+++ Blood_Bank/Blood_bank_app/views.py
@@ -3,7 +3,9 @@ from Blood_bank_app.models import DonorDetails
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import JsonResponse
+
 # Create your views here.
+
 def home(request):
     return redirect('login')
 
@@ -15,8 +17,7 @@ def log_in(request):
         password = request.POST['password']
         user = auth.authenticate(username=username,password=password)
         if user is not None:
-            session_valid = True
-            request.session['session_valid'] = session_valid
+            request.session['session_valid'] = username
             return JsonResponse(
                 {'success':True},
                 safe = False
@@ -30,33 +31,40 @@ def log_in(request):
         return render(request,'blood_login.html')
 
     
-def sign_in(request):
-
-    if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        email = request.POST['email']
-        username = request.POST['username']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-
-        if password1 == password2:
-            if User.objects.filter(username=username).exists():
-                messages.info(request, 'username already taken')
-                return redirect('signin')
-            elif User.objects.filter(email=email).exists():
-                messages.info(request,'email already taken')
-                return redirect('signin')
+def sign_up(request):
+        if request.method == 'POST':
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            email = request.POST['email']
+            username = request.POST['username']
+            password1 = request.POST['password1']
+            password2 = request.POST['password2']
+    
+            if password1 == password2:
+                if User.objects.filter(username=username).exists():
+                    return JsonResponse(
+                        {'success':'user_error'},
+                        safe = False
+                    )
+                elif User.objects.filter(email=email).exists():
+                    return JsonResponse(
+                        {'success':'email_error'},
+                        safe = False
+                    )
+                else:
+                    user = User.objects.create_user(first_name=first_name,last_name=last_name,password=password1,username=username,email=email)
+                    user.save()
+                    return JsonResponse(
+                        {'success':'True'},
+                        safe = False
+                    )
             else:
-                user = User.objects.create_user(first_name=first_name,last_name=last_name,password=password1,username=username,email=email)
-                user.save()
-                messages.info(request,'user created')
-                return redirect('login')
-        else:
-            messages.info(request,'password not matching...')
-            return redirect(request,'signin')
-    else: 
-        return render(request,'blood_signup.html')
+                return JsonResponse(
+                    {'success': 'password_error'},
+                    safe = False
+                )
+        else: 
+            return render(request,'blood_signup.html')
 
 
 def display(request):
@@ -72,16 +80,31 @@ def adddonor(request):
             blood_group = request.POST['blood_group']
             age = request.POST['age']
             phone_number = request.POST['phone_number']
-            details = DonorDetails.objects.create(name=name,blood_group=blood_group,age=age,phone_number=phone_number)
-            details.save()
-            return redirect('display')
+            if DonorDetails.objects.filter(phone_number=phone_number).exists():
+                return JsonResponse(
+                    {'success':'phonenumber_error'},
+                    safe = False
+                )
+            elif DonorDetails.objects.filter(name=name).exists():
+                return JsonResponse(
+                    {'success':'name_error'},
+                    safe = False
+                )
+            else:
+                details = DonorDetails.objects.create(name=name,blood_group=blood_group,age=age,phone_number=phone_number)
+                details.save()
+                return JsonResponse(
+                    {'success':'true'},
+                    safe = False
+                )
         else:
             return render(request, 'Blood_home.html')
 
 
 def logout(request):
-    try:
-        del request.session['password']
-    except KeyError:
-        pass
-    return redirect('login')
+    if 'session_valid' in request.session :
+        try:
+            del request.session['session_valid']
+        except KeyError:
+            pass
+        return redirect('login')
